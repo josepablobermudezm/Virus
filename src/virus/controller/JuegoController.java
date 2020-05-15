fe17b14bb1500bc6ca5cc3bbc11b57930f9c9141
josepablobermudezm <53883209+josepablobermudezm@users.noreply.github.com>
2020-05-14 18:39:47 -0600

    act #19

--- src/virus/controller/JuegoController.java
+++ src/virus/controller/JuegoController.java
@@ -12,6 +12,7 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
 import java.net.UnknownHostException;
@@ -19,6 +20,7 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -34,6 +36,7 @@ import javafx.scene.shape.Rectangle;
 import static virus.controller.InicioController.enviarObjetos;
 import virus.model.CartaDto;
 import virus.model.JugadorDto;
+import virus.model.PartidaDto;
 import virus.util.AppContext;
 import virus.util.FlowController;
 import virus.util.Hilo;
@@ -133,6 +136,7 @@ public class JuegoController extends Controller implements Initializable {
     public CartaDto carta3, cartaAux;
     @FXML
     private ImageView imgDesechada;
+    private PartidaDto partida = new PartidaDto();
 
     /**
      * Initializes the controller class.
@@ -195,6 +199,41 @@ public class JuegoController extends Controller implements Initializable {
         anchor.getChildren().add(image7);
         anchor.getChildren().add(image8);
         anchor.getChildren().add(image9);
+
+        DataInputStream entrada;
+        DataOutputStream salida;
+        Socket socket;
+        ServerSocket serverSocket;
+        String mensajeRecibido;
+
+        while (true) {
+            try {
+                serverSocket = new ServerSocket(44440);
+                System.out.println("Esperando una conexión...");
+                socket = serverSocket.accept();
+                System.out.println("Un cliente se ha conectado...");
+                entrada = new DataInputStream(socket.getInputStream());
+                System.out.println("Confirmando conexion al cliente....");
+                mensajeRecibido = entrada.readUTF();
+                System.out.println(mensajeRecibido);
+                if ("cartaDesechada".equals(mensajeRecibido)) {
+                    DataInputStream respuesta2 = new DataInputStream(socket.getInputStream());
+                    ObjectInputStream objectInputStream = new ObjectInputStream(respuesta2);
+                    CartaDto carta = (CartaDto) objectInputStream.readObject();
+                    partida.getDesechadas().add(carta);
+                    Platform.runLater(() -> {
+                        imgDesechada.setImage(new Image("virus/resources/" + cartaAux.getImagen()));
+                    });
+                } else if ("carta".equals(mensajeRecibido)) {
+
+                }
+                serverSocket.close();
+            } catch (IOException IO) {
+                System.out.println(IO.getMessage());
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(JuegoController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     EventHandler<MouseEvent> cartaAdesechar = event -> {
@@ -266,7 +305,6 @@ public class JuegoController extends Controller implements Initializable {
     private void CartaDesechada(MouseEvent event) {
 
         if (cartaAux != null) {
-            imgDesechada.setImage(new Image("virus/resources/" + cartaAux.getImagen()));
             try {
                 jugador.getMazo().remove(cartaAux);
                 Socket socket = new Socket(jugador.getIPS(), 44440);
